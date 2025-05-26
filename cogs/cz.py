c4ec5f144a0892ecafde0a7315a08c4e1f7a2018
kassh7 <kasshmmo@gmail.com>
2025-05-26 20:25:56 +0200

    add cz

---
+++ cogs/cz.py
@@ -0,0 +1,64 @@
+import datetime
+import os
+import random
+
+from discord.utils import get
+from discord.ext import commands, tasks
+
+
+class Cz(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command(name="cz", with_app_command=True,
+                             description="czegenyszki")
+    async def command_cz(self, ctx):
+        try:
+            if ctx.guild is None or int(ctx.guild.id) != int(os.getenv("CZ_SERV")):
+                return
+
+            author = ctx.author.display_name
+            #if self.bot.state.is_expired("cz"): // todo
+            newnick = self.get_breveg
+            #self.bot.state.add_timeout("cz", expiry_td=datetime.timedelta(minutes=1))
+            member = get(ctx.channel.members, id=int(os.getenv("CZ")))
+            await member.edit(nick=newnick)
+            await ctx.response.send_message(f"{author} szerint: {member.mention}")
+            #else:
+            #    await ctx.response.send_message("pill...")
+        except Exception as e:
+            print(f"baj van: {e}")
+            await ctx.send(f"baj van: {e}")
+
+    @property
+    def get_breveg(self):
+        consonants = [char for char in "bcdfghjklmnpqrstvwxz"] + ["gy", "cz", "dzs", "ty", "br", "cs"]
+        prebuilts = [
+            "hét", "gét", "rét", "új", "már", "gép", "tér", "vér", "zágráb", "zárt", "kétabony", "hosszú", "bánat",
+            "dér"
+        ]
+        enders = [
+            "végi", "helyi", "ési", "réti", "gényi", "esi", "melletti", "közi", "kerti", "faszú", "téri", "falvi",
+            "fejű"
+        ]
+
+        out = ""
+        if random.choice([True, False]):
+            pre = random.choice(prebuilts)
+            out += pre
+            if pre == "dér" and random.randrange(0, 100) > 40:
+                out += "heni"
+            else:
+                out += random.choice(enders)
+        else:
+            start = random.choice(consonants) + "é"
+            end = random.choice(enders)
+            mid = random.choice(consonants) if (start[-1:] not in consonants and end[:-1] not in consonants) \
+                                               or random.choice([True, False]) else ""
+            out = f"{start}{mid}{end}"
+
+        return out.capitalize()
+
+
+async def setup(bot):
+    await bot.add_cog(Cz(bot))
