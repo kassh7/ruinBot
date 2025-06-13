58f89dc7bdcc067a2366e4c73e1228429baaa39c
neonoxd <neonoxd14@gmail.com>
2025-06-13 14:23:47 +0200

    jerma985

--- cogs/kifogas.py
+++ cogs/kifogas.py
@@ -1,6 +1,8 @@
 import json
 import random
 import os
+import discord
+import random
 
 from discord.ext import commands
 
@@ -10,12 +12,17 @@ class Kifogas(commands.Cog):
         self.bot = bot
         with open("res/kifogasok.json", "r", encoding='utf-8') as f:
             self.kifogas_json = json.load(f)
+        
+        @bot.tree.context_menu(name="jerma985")
+		async def nyomod(interaction: discord.Interaction, message: discord.Message):
+			await message.reply(random.choice(["-","+"]) + str(random.randrange(1,3)))
+			await interaction.response.send_message(content="hehe", ephemeral=True, delete_after=1)
 
     @commands.hybrid_command(name="kifogas", with_app_command=True,
                              description="kifogás generátor")
     async def kifogas(self, ctx):
         kifogas = await self.generate_kifogas(ctx)
-        await ctx.reply(kifogas)
+        await ctx.reply(kifogas)random.choice(["-","+"])+str(random.randrange(1,3))
 
     async def generate_kifogas(self, ctx):
         kifogas_affix = random.choice(self.kifogas_json['prefixes'])
